f6fe4c50f48ed02cf098e43bc4d5ba30c24b0a9e
DmitriyPetunin <154313405+DmitriyPetunin@users.noreply.github.com>
2024-12-21 19:24:56 +0300

    fix db connection

--- src/main/java/ru/kpfu/itis/util/ConnectionProvider.java
+++ src/main/java/ru/kpfu/itis/util/ConnectionProvider.java
@@ -19,9 +19,15 @@ public class ConnectionProvider {
     }
 
     public Connection getCon() {
+        String PROD_DB_HOST = System.getenv(  "PROD_DB_HOST");
+        String PROD_DB_PORT = System. getenv(  "PROD_DB_PORT");
+        String PROD_DB_PASSWORD = System.getenv (  "PROD_DB_PASSWORD");
+        String PROD_DB_NAME = System. getenv(  "PROD_DB_NAME");
+        String PROD_DB_USERNAME = System.getenv(  "PROD_DB_USERNAME");
         try {
             Class.forName("org.postgresql.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres","postgres","password");
+            con = DriverManager.getConnection("jdbc:postgresql://%s:%s/%s".formatted(PROD_DB_HOST,PROD_DB_PORT,PROD_DB_NAME),PROD_DB_USERNAME,PROD_DB_PASSWORD);
+
         } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
